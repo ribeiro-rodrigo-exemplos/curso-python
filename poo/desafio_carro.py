dbbd18da06276e65b5268e933815a8648cba9c29
Rodrigo Ribeiro <ribeiro.rodrigo1989@gmail.com>
2019-06-06 07:29:19 -0300

    seguindo o capitulo sobre poo

---
+++ poo/desafio_carro.py
@@ -0,0 +1,30 @@
+class Carro:
+    def __init__(self, velocidade_maxima):
+        self.velocidade_maxima = velocidade_maxima
+        self.velocidade_atual = 0
+
+    def acelerar(self, delta=5):
+        maxima = self.velocidade_maxima
+        nova = self.velocidade_atual + delta
+
+        self.velocidade_atual = nova if nova <= maxima else maxima
+
+        return self.velocidade_atual
+
+    def frear(self, delta=5):
+        minima = 0
+        nova = self.velocidade_atual - delta
+
+        self.velocidade_atual = nova if nova >= minima else minima
+
+        return self.velocidade_atual
+
+
+if __name__ == '__main__':
+    c1 = Carro(180)
+
+    for _ in range(25):
+        print(c1.acelerar(8))
+
+    for _ in range(10):
+        print(c1.frear(delta=20))
